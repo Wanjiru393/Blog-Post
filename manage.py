c31d68ae41b16605c3004039ac1672d522101aab
Wanjiru393 <carolciru393@gmail.com>
2022-05-15 09:13:13 +0300

    Created Basic templates

--- manage.py
+++ manage.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template
-
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-
-
+posts = [
+    {
+        'author': 'Wanjiru Samuels',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html')
-
-@app.route("/blog")
-def blog():
-    return "<p>Hey You!</p>"
+    return render_template('index.html', posts=posts)
 
 
-if __name__ == "__main__":
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
