7280874641e163aa848b985a24adc1663e57b250
Wanjiru393 <carolciru393@gmail.com>
2022-05-16 07:40:54 +0300

    Created postform class

--- app/views.py
+++ app/views.py
@@ -1,29 +1,15 @@
 from flask import  render_template, url_for, flash,redirect
 from app import app, db, bcrypt
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm, PostForm
 from .models import User,Post
 from flask_login import login_user,current_user, logout_user, login_required
 
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 
@@ -73,3 +59,17 @@ def logout():
 def account():
     return render_template('account.html', title='Account')
 
+#POSTS
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Blog Post has been created !', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
