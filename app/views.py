3f2e5280bc624f475411ea359daa09083ed20ee0
Wanjiru393 <carolciru393@gmail.com>
2022-05-17 13:19:11 +0300

    Added read order from recent

--- app/views.py
+++ app/views.py
@@ -10,7 +10,8 @@ import requests
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page',1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()) .paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts)
 
 
