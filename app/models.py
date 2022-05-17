d3191ee6a4caf21496e2ff8ede3fad6b5eaf402e
Wanjiru393 <carolciru393@gmail.com>
2022-05-17 18:03:35 +0300

    deploy

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
 
     @staticmethod
     def get_reset_token(self,expires_sec=1800):
@@ -32,7 +33,7 @@ class User(db.Model, UserMixin):
             return None
         return User.query.get(user_id)
 
-    # comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    # 
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -44,8 +45,6 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-   
-
     # comment = db.relationship('Comment', backref='post', lazy='dynamic')
 
 
